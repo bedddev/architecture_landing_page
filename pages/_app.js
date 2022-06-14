c886c783ca89742110d9fa8b4d4967b86aa6fc47
Bedd <bedddev18@gmail.com>
2022-06-14 15:48:45 +0700

    change  app

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Arx - Architecture Landing Page</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
           rel="stylesheet"
